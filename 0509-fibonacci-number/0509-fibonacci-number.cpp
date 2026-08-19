01923321a6a3a27e1a80b3c30785a444d04062cd
Narendra Wagh <nawagh758@gmail.com>
2026-08-19 22:08:08 +0530

    Time: 0 ms (100%), Space: 7.7 MB (88.84%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
     int fib(int n) {
-        int r;
+        int r,a=0,b=1;
         if(n==0) return 0;
         if(n==1) return 1; 
         if(n>1)
         {   
-            r=fib(n-1)+fib(n-2);
-            
+            for(int i=2;i<=n;i++)
+            {
+                r=a+b;
+                a=b;
+                b=r;
 
+            }
+            
         }
         return r;
     }
